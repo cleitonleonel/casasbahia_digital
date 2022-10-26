989a3f29230deb5cbe962339a3907dd7a1eab246
cleiton <cleiton.leonel@gmail.com>
2022-10-26 17:25:17 -0300

    Atualização do código para correção de bugs devido as mudanças recentes feitas na comunicação via websocket.

--- fatura_digital.py
+++ fatura_digital.py
@@ -8,6 +8,7 @@ from optparse import OptionParser
 wss_url = re.findall(r"var socket_url = '(.*?)';",
                      requests.get("https://prime.altubots.com/chats/viavarejo/1/index.html").text)[0]
 identifier = wss_url.split("=")[-1]
+source_id = None
 
 
 def client_reply(message, is_private=False, initial_reply=None):
@@ -16,8 +17,8 @@ def client_reply(message, is_private=False, initial_reply=None):
           if is_private else message) if message != "" else ""
     return {
         "source": "widget", "event": "chat_message", "params": initial_reply if initial_reply else {
-            "slug": "viavarejo", "identifier": identifier, "assistant_id": "1",
-            "widget_id": "1", "livechat": False, "homol": False
+            "slug": "viavarejo", "source": "widget", "source_id": source_id,
+            "identifier": identifier, "homol": False
         },
         "data": {
             "message": message,
@@ -27,44 +28,57 @@ def client_reply(message, is_private=False, initial_reply=None):
 
 
 def on_message(ws, message):
+    global source_id
     data = json.loads(message)
 
     if data.get("event") == "connected":
         reply = {
-            "slug": "viavarejo", "identifier": data["data"].get("identifier"),
-            "assistant_id": "1", "widget_id": "1", "url_params": {
-                "identifier": identifier,
-                "slug": "viavarejo", "source": "widget",
-                "widget_identifier": data["data"]["url_params"]["widget_identifier"],
-                "connection_id": data["data"]["url_params"]["connection_id"],
-                "ip": data["data"]["url_params"]["ip"],
-                "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                              "Chrome/96.0.4664.110 Safari/537.36"},
-            "livechat": False, "homol": False
+            "source": "widget",
+            "event": "chat_message",
+            "params": {
+                "slug": "viavarejo",
+                "source": "widget",
+                "source_id": data["data"]["params"]["source_id"],
+                "identifier": data["data"]["params"]["identifier"],
+                "homol": False,
+                "init": {
+                    "source": "widget",
+                    "ip": data["data"]["params"]["init"]["ip"],
+                    "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                  "Chrome/96.0.4664.110 Safari/537.36",
+                    "external_id": data["data"]["params"]["init"]["external_id"]
+                },
+            },
+            "data": {
+                "message": "",
+                "cognitive": False
+            }
         }
-        reply = client_reply("", initial_reply=reply)
+
+        source_id = data["data"]["params"]["source_id"]
+        # reply = client_reply("", initial_reply=reply)
         ws.send(json.dumps(reply))
 
     elif data.get("event") == "chat_message":
-        print("==> Server: ", data["data"][0]["text"])
-        if data["data"][0]["text"] in ['Olá! Sou a Ana, Assistente Virtual das Casas Bahia. Posso te ajudar com a '
-                                       'fatura do seu cartão Casas Bahia.']:
+        print("==> Server: ", data["data"]["messages"][0]["text"])
+        if data["data"]["messages"][0]["text"] in ['Olá! Sou a Ana, Assistente Virtual das Casas Bahia. '
+                                                   'Posso te ajudar com a fatura do seu cartão Casas Bahia.']:
             reply = client_reply("Sim")
             ws.send(json.dumps(reply))
 
-        elif data["data"][0]["text"] in ['OK, vamos lá. Qual o seu CPF?',
-                                         'Desculpe mas este CPF é inválido. Tente novamente por favor.']:
+        elif data["data"]["messages"][0]["text"] in ['OK, vamos lá. Qual o seu CPF?',
+                                                     'Desculpe mas este CPF é inválido. Tente novamente por favor.']:
             reply = client_reply(cpf, is_private=True)
             ws.send(json.dumps(reply))
 
-        elif data["data"][0]["text"] in ['Por favor informe os quatro últimos dígitos do seu cartão.',
-                                         'Desculpe, mas não consegui verificar seus dados.']:
+        elif data["data"]["messages"][0]["text"] in ['Por favor informe os quatro últimos dígitos do seu cartão.',
+                                                     'Desculpe, mas não consegui verificar seus dados.']:
             reply = client_reply(last_digits, is_private=True)
             ws.send(json.dumps(reply))
 
-        elif 'segue a sua última fatura fechada' in data["data"][0]["text"]:
+        elif 'segue a sua última fatura fechada' in data["data"]["messages"][0]["text"]:
             link_pdf = re.compile(r"<a href=\'(.+?)\' target=\'_blank\'>Baixar Fatura</a>").findall(
-                data["data"][2]["text"])[0]
+                data["data"]["messages"][2]["text"])[0]
             print(f"\n{link_pdf}")
             file_path = 'fatura_casas_bahia.pdf'
             with requests.get(link_pdf, stream=True) as response:
@@ -72,12 +86,12 @@ def on_message(ws, message):
                 with open(file_path, 'wb') as out_file:
                     for chunk in response.iter_content(chunk_size=1024 * 1024):
                         out_file.write(chunk)
-            print(f'\n{data["data"][1]["text"]}')
+            print(f'\n{data["data"]["messages"][1]["text"]}')
             ws.close()
 
-        elif data["data"][0]["text"] in ['Desculpe mas o CPF que você está tentando parece inválido. Por favor '
-                                         'verifique e tente mais tarde.', 'Não foi possível localizar os dados da sua'
-                                                                          ' fatura.']:
+        elif data["data"]["messages"][0]["text"] in ['Desculpe mas o CPF que você está tentando parece inválido. '
+                                                     'Por favor verifique e tente mais tarde.',
+                                                     'Não foi possível localizar os dados da sua fatura.']:
             ws.close()
     time.sleep(0.1)
 
@@ -93,6 +107,7 @@ if __name__ == "__main__":
 
     (options, args) = parser.parse_args()
     cpf, last_digits = options.cpf, options.last_digits
+    # websocket.enableTrace(True)
     ws = websocket.WebSocketApp(f"{wss_url}&identifier={identifier}",
                                 on_message=on_message
                                 )
